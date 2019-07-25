f5c6393fabcd77ebf1c21ed79a7334a000b3127a
sasha.dasha001 <avoitovas@gmail.com>
2019-07-25 21:37:36 +0300

    small fix on last variable

--- football_test.py
+++ football_test.py
@@ -1,9 +1,8 @@
-import requests
-import pandas as pd
-import lxml
 from lxml import html
-import re
-myurl = 'https://www.flashscore.com/football/england/premier-league-2018-2019/results/'
-page_cont=requests.get(myurl)
-tree = html.fromstring(page_cont.content)
-url.parts = tree.xpath('//div[starts-with(@id,"g_1")]')
+import requests
+
+page = requests.get('https://www.flashscore.com/football/england/premier-league-2018-2019/results/')
+tree = html.fromstring(page.content)
+
+out = tree.xpath("//div[@class='sportName soccer']//@id")
+print(out)
