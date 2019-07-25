382bf3832876382335e46351265d656e54b61ac6
sasha.dasha001 <avoitovas@gmail.com>
2019-07-25 22:52:55 +0300

    add match summary links

--- football_test.py
+++
@@ -1,8 +0,0 @@
-from lxml import html
-import requests
-
-page = requests.get('https://www.flashscore.com/football/england/premier-league-2018-2019/results/')
-tree = html.fromstring(page.content)
-
-out = tree.xpath("//div[@class='sportName soccer']//@id")
-print(out)
